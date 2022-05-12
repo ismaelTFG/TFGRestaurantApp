67fe9e69b2859769654329aea3077696a6363ce6
MONICO SIENES Ismael <ismael.monico@soprasteria.com>
2022-05-12 16:19:02 +0200

    vistas comunes sin conexion

---
+++ app/src/main/java/com/eep/tfgrestaurantapp/ConRedActivity.kt
@@ -0,0 +1,14 @@
+package com.eep.tfgrestaurantapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.eep.tfgrestaurantapp.R
+
+class ConRedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_con_red)
+    }
+
+}
